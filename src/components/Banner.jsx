5ac1a24677bb9eb6705bcd4cc749f4db8a7bccf0
Luciano Vales <lvales73@gmail.com>
2022-12-03 11:15:51 -0300

    Correcciones Banner

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -2,19 +2,21 @@ import Category from "./Category"
 
 const Banner = () => {
    return (
-      <div className="flex justify-between bg_banner mb-32">
-         <div className="relative mx-auto my-auto">
-            <div className="ml-20">
+      <>
+         <div className="grid grid-cols-11 bg_banner mb-32">
+            <div className="col-span-6 mx-auto my-auto">
                <div className="text-4xl font-normal text-white">¡Apurate! Oferta por tiempo limitado.</div>
                <div className="text-7xl font-semibold text-white mt-3">Rebajas en ropa deportiva</div>
                <div className="text-2xl font-normal text-white mt-3">Zapatillas, keds, remeras y muccho más...</div>
+            </div>
+            <div className="col-span-5">
+               <img src="../assets/img/banner.jpg" alt="banner" />
             </div>
          </div>
          <div className="absolute bottom-32 left-1/4">
             <Category />
          </div>
-         <img src="../assets/img/banner.jpg" alt="banner" />
-      </div>
+      </>
    )
 }
 
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -8,7 +8,7 @@ const NavBar = () => {
                <svg xmlns="http://www.w3.org/2000/svg" className="h-10 w-10" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
                   <path strokeLinecap="round" strokeLinejoin="round" d="M16 11V7a4 4 0 00-8 0v4M5 9h14l1 12H4L5 9z" />
                </svg>
-               <a className="text-3xl font-black text-slate-700" href="/">Shop</a>
+               <a className="text-3xl font-black text-slate-700" href="/"><span className="text-red-600 text-4xl">.</span>Shop</a>
             </div>
             <nav className="flex gap-5 items-center text-lg font-medium">
                <a href="#/" className="hover:text-slate-500">Hombre</a>
