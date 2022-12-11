84ce80a7e6b5e78d224b78608239b10d8845c5bc
Luciano Vales <lvales73@gmail.com>
2022-12-11 17:41:19 -0300

    Componente ItemListContainer

--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -1,9 +1,30 @@
+import ItemCount from "./ItemCount";
+import ItemList from "./ItemList";
+import products from "../data/productsMl.json";
+import { useEffect, useState } from "react";
+
 const ItemListContainer = ({ mensaje }) => {
+
+   const [items, setItems] = useState([]);
+
+   useEffect(() => {
+      const getProducts = new Promise((resolve) => {
+         setTimeout(() => {
+            resolve(products);
+         }, 2000);
+      });
+      getProducts.then((result) => {
+         setItems(result);
+      });
+   }, []);
+
    return (
-      <div>
-         <p className="mt-10 text-xl lg:text-5xl text-slate-600 text-center lg:p-10 lg:mt-32 font-bold">{mensaje}</p>
-      </div>
+      <>
+         <p className="mt-10 text-xl lg:text-5xl text-slate-600 text-center lg:p-10 lg:mt-48 font-bold">{mensaje}</p>
+         <ItemList items={items}/>
+         <ItemCount InitialStock={10} />
+      </>
    );
 }
 
-export default ItemListContainer;
+export default ItemListContainer; 
