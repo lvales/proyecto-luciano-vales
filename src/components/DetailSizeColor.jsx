b7f96e4a11290dca3f127e11b705db1311d04fd1
Luciano Vales <lvales73@gmail.com>
2022-12-20 11:40:40 -0300

    DetalSizeColor y StarRanking

---
+++ src/components/DetailSizeColor.jsx
@@ -0,0 +1,39 @@
+import '../data/clothesColors.css'; 
+
+const DetailSizeColor = ({ sizes, colors }) => {
+
+   return (
+      <>
+         <div className="flex my-5">
+            <p className="w-1/2">Talles:</p>
+            <p className="w-1/2">Colores:</p>
+         </div >
+         <div className="flex">
+            <div className="flex w-1/2">
+               {
+                  sizes?.map((size, id) => {
+                     return (
+                        <button className="border border-slate-800 rounded-md w-9 text-center m-1 uppercase inline-block align-middle hover:border-red-400 hover:bg-red-400 hover:text-white active:border-red-400 selection:bg-red-400" key={id}>
+                           {size}
+                        </button>
+                     );
+                  })
+               }
+            </div>
+            <div className="flex w-1/2">
+               {
+                  colors?.map((color, id) => {
+                     const style = "mr-2 border-2 border-slate-400 rounded-full p-4 " + color;
+                     console.log(style);
+                     return (
+                        <div key={id} className={ style }></div>
+                     );
+                  })
+               }
+            </div>
+         </div>
+      </>
+   )
+}
+
+export default DetailSizeColor
--- src/components/StarsRanking.jsx
+++ src/components/StarsRanking.jsx
@@ -2,7 +2,7 @@ import { IoStarHalf, IoStarOutline, IoStarSharp } from 'react-icons/io5';
 
 const StarsRanking = ({stars, numberStars}) => {
   return (
-   <>
+   <div className="flex items-center mt-2.5 mb-5">
       {
          stars?.map((star, id) => (star === 1) ?
             <IoStarSharp className='text-yellow-400 text-lg' key={id} />
@@ -12,7 +12,7 @@ const StarsRanking = ({stars, numberStars}) => {
          )
       }
       <span className="bg-blue-100 text-blue-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded ml-3">{numberStars}</span>
-   </>
+   </div>
   )
 }
 
